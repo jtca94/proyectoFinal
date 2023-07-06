d7a60eeb9a14d8788e6e501bfdb886f9e90f5a2f
jtcannobbio <tomas.cannobbio@gmail.com>
2023-07-06 12:39:52 -0400

    added theme and typography

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,13 +1,40 @@
+/* eslint-disable react/no-unknown-property */
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import {BrowserRouter} from "react-router-dom";
+import { themeProvider} from "@mui/material";
+
+const theme = themeProvider({
+  palette: {
+    primary: {
+      main: "#001d3d",
+    },
+    secondary: {
+      main: "#ffc300",
+    },
+    custom: {
+      darkBlue: "#000814",
+      blue: "#003566",
+      yellow: "#ffd60a",
+    }
+  },
+  typography: {
+    fontFamily: "Roboto",
+    fontWeightLight: 300,
+    fontWeightRegular: 400,
+    fontWeightMedium: 500,
+    fontWeightBold: 700,
+  }
+});
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
+      <themeProvider theme={theme}>
       <App />
+      </themeProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
