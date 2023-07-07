dbaf83d8d4daa30035eee5900f054b17fe08ecd4
jtcannobbio <tomas.cannobbio@gmail.com>
2023-07-07 10:56:12 -0400

    initial login and register page

--- client/src/pages/LoginRegister/Login.jsx
+++ client/src/pages/LoginRegister/Login.jsx
@@ -1,10 +1,45 @@
-import { Container } from "@mui/material"
+import {Button, Container, Grid, TextField, Typography} from "@mui/material";
+import {Link} from "react-router-dom";
 
 const Login = () => {
   return (
-    <Container maxWidth="lg">
-      <h1>Login</h1>
+    <Container maxWidth="lg" sx={{my: 20}}>
+      <Typography variant="h3" fontWeight={500} sx={{textAlign: "center"}}>
+        Inicia Sesión
+      </Typography>
+      <Grid container sx={{mt: 5, justifyContent: "center"}}>
+        <Grid item xs={12} md={6}>
+          <form>
+            <TextField
+              fullWidth
+              label="Email"
+              variant="outlined"
+              placeholder="email@mail.com"
+            />
+            <TextField
+              fullWidth
+              label="Contraseña"
+              type="password"
+              variant="outlined"
+              sx={{mt: 5}}
+              placeholder="********"
+            />
+            <Typography variant="body1" sx={{mt: 3, textAlign: "center"}}>
+              ¿No tienes una cuenta? <Link to="/register">Regístrate</Link>
+            </Typography>
+            <Button
+              color="secondary"
+              variant="contained"
+              fullWidth
+              sx={{mt: 3}}
+              size="large"
+            >
+              Iniciar Sesión
+            </Button>
+          </form>
+        </Grid>
+      </Grid>
     </Container>
-  )
-}
-export default Login
+  );
+};
+export default Login;
--- client/src/pages/LoginRegister/Register.jsx
+++ client/src/pages/LoginRegister/Register.jsx
@@ -1,6 +1,77 @@
+import {Container, Grid, Typography, TextField, Button} from "@mui/material";
+
 const Register = () => {
   return (
-    <div>Register</div>
-  )
-}
-export default Register
+    <Container maxWidth="lg" sx={{my: 10}}>
+      <Typography variant="h3" fontWeight={500} sx={{textAlign: "center"}}>
+        Crea tu Usuario
+      </Typography>
+      <Grid container sx={{justifyContent: "center"}}>
+        <Grid item xs={12} md={6}>
+          <form>
+            <TextField
+              fullWidth
+              label="Nombre de Usuario"
+              variant="outlined"
+              sx={{mt: 3}}
+              placeholder="Juan"
+            />
+            <TextField
+              fullWidth
+              label="Nombre"
+              variant="outlined"
+              sx={{mt: 3}}
+              placeholder="Juan"
+            />
+            <TextField
+              fullWidth
+              label="Apellido"
+              variant="outlined"
+              sx={{mt: 3}}
+              placeholder="González"
+            />
+            <TextField
+              fullWidth
+              label="Email"
+              variant="outlined"
+              sx={{mt: 3}}
+              type="email"
+              placeholder="myemail@mail.com"
+            />
+            <TextField
+              fullWidth
+              type="password"
+              label="Contraseña"
+              variant="outlined"
+              sx={{mt: 3}}
+            />
+            <TextField
+              fullWidth
+              type="password"
+              label="Repite tu contraseña"
+              variant="outlined"
+              sx={{mt: 3}}
+            />
+            <TextField
+              fullWidth
+              label="Domicilio"
+              variant="outlined"
+              sx={{mt: 3}}
+              placeholder="Av. Siempre Viva 123"
+            />
+            <Button
+              color="secondary"
+              variant="contained"
+              fullWidth
+              size="large"
+              sx={{mt: 3}}
+            >
+              Crear Usuario
+            </Button>
+          </form>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
+export default Register;
