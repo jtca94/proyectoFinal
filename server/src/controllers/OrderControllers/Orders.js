87c470fa7b7d951b245030231706763ba6b36013
jtcannobbio <tomas.cannobbio@gmail.com>
2023-07-16 21:25:37 -0400

    restored backend files

--- server/src/controllers/OrderControllers/Orders.js
+++ server/src/controllers/OrderControllers/Orders.js
@@ -1,7 +1,7 @@
 import {addOrder} from "../../models/OrdersModels/addOrder.js";
 import {handleErrors} from "../../middlewares/handleErrors.js";
 
-export const add = async (req, res) => {
+const newOrder = async (req, res) => {
   try {
     const {userid, productid, quantity} = req.body;
     addOrder(userid, productid, quantity);
@@ -10,4 +10,8 @@ export const add = async (req, res) => {
     const {status, message} = handleErrors(error.code);
     return res.status(status).json({ok: false, message: message});
   }
+};
+
+export const orderControllers = {
+  newOrder,
 };
--- server/src/routes/routes.js
+++ server/src/routes/routes.js
@@ -1,7 +1,13 @@
 import {Router} from "express";
+//controllers
 import {userControllers} from "../controllers/UserControllers/Users.js";
 import {productControllers} from "../controllers/ProductsControllers/products.js";
+import {orderControllers} from "../controllers/OrderControllers/orders.js";
+import { ratingControllers } from "../controllers/RatingsControllers/Ratings.js";
+//middlewares
 import {passwordHash} from "../middlewares/passwordHash.js";
+import {verifyOrders} from "../middlewares/verifyOrders.js";
+import { verifyRatings } from "../middlewares/verifyRatings.js";
 import {verifyNewUser} from "../middlewares/verifyNewUser.js";
 import {verifyCredentials} from "../middlewares/verifyCredentials.js";
 import {verifyNewProduct} from "../middlewares/verifyNewProduct.js";
@@ -13,6 +19,8 @@ const routes = Router();
 routes.post("/register", verifyNewUser, passwordHash, userControllers.newUser);
 routes.post("/login", verifyCredentials, userControllers.LoginUser);
 routes.post("/products", verifyNewProduct, productControllers.createProduct);
+routes.post('/orders',verifyOrders, orderControllers.newOrder );
+routes.post('/ratings/:productid', verifyRatings, verifyToken, ratingControllers.setRating);
 //GET ROUTES
 routes.get("/productsByUser", verifyToken, productControllers.UserProducts);
 routes.get("/products", productControllers.getProducts);
